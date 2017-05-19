576f9b15f074a3a92466cd9ca0f721ae2c287f61
Grzegorz Musial <ext-grzegorz.musial@f-secure.com>
2017-05-19 09:42:32 +0200

    working dynamodb

--- Serverless_Coffee/app/index.js
+++ Serverless_Coffee/app/index.js
@@ -3,3 +3,9 @@
 module.exports.get = (event, context, callback) => {
   callback(null, {statusCode: 200, body: JSON.stringify({text: 'yay!'})});
 };
+
+module.exports.onDbChange = (event, context, callback) => {
+  console.log(event);
+  console.log(event.Records[0].dynamodb);
+  callback(null, {statusCode: 200, body: event});
+};
